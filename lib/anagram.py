31c76705ef31e19f8053703eb5617633b0350e96
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-24 10:28:00 -0500

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+
+    def match(self, input):
+        sorted_chars = sorted([char for char in self.word])
+        result = []
+        
+        for word in input:
+            if sorted_chars == sorted([char for char in word]):
+                result.append(word)
+        
+        return result
